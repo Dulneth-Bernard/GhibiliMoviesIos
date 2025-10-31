52b6dfaa863ab3740ff8830b6a5a9a1b24aff046
Dulneth Bernard <dulneth.bernard@gmail.com>
2025-10-31 16:30:19 +0530

    feat: decoupled loading states

---
+++ GhibiliMovieApp/Core/Networking/LoadingState.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingState.swift
+//  GhibiliMovieApp
+//
+//  Created by Dulneth Bernard on 31/10/2025.
+//
+
+import Foundation
---
+++ GhibiliMovieApp/Core/Networking/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  GhibiliMovieApp
+//
+//  Created by Dulneth Bernard on 31/10/2025.
+//
+
+import Foundation
