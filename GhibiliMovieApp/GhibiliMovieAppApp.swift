654566acbb251f4fa327acdd3bc1512556f2403d
Dulneth Bernard <dulneth.bernard@gmail.com>
2025-10-31 15:20:52 +0530

    Initial Commit

---
+++ GhibiliMovieApp/GhibiliMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibiliMovieAppApp.swift
+//  GhibiliMovieApp
+//
+//  Created by Dulneth Bernard on 31/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct GhibiliMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
